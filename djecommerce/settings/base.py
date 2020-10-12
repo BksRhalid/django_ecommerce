f01bf66e62a0bd2d9d51ba586d94afce9ba51058
rhalid <rhalid.bouakhris@gmail.com>
2020-10-12 04:52:02 +0200

    settings updated database

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -86,6 +86,13 @@ LOGIN_REDIRECT_URL = '/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 if os.environ.get('ENV') == 'PRODUCTION':
     ALLOWED_HOSTS = ['soldorak.herokuapp.com']
     # Static files settings
