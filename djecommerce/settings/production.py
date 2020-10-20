622bb23054a235a73d632072b54cd76219bfb3b0
rhalid <rhalid.bouakhris@gmail.com>
2020-10-20 17:44:24 +0200

    changed to manage.py for production static

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '127.0.0.1']
 # ALLOWED_HOSTS = ['soldorak.herokuapp.com']
 
 
@@ -27,15 +27,15 @@ DATABASES = {
 STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
 
-# Static files settings
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# # Static files settings
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static_in_env'),
-)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static_in_env'),
+# )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
