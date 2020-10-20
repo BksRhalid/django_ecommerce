722468a4b7b7b93b99e5b823906ee5fc4de4aa0e
rhalid <rhalid.bouakhris@gmail.com>
2020-10-20 21:39:51 +0200

    updated setting media

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -65,13 +65,25 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+
+# Static files settings
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_in_env'),
+]
 
 # Auth
 
