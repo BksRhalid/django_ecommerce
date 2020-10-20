0d1f3246de82fcb29fff31398dcb23b9918be117
rhalid <rhalid.bouakhris@gmail.com>
2020-10-20 15:44:07 +0200

    changed to manage.py for production

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -1,163 +1,163 @@
-import os
-from decouple import config
-import dj_database_url
-# import django_heroku
+# import os
+# from decouple import config
+# import dj_database_url
+# # import django_heroku
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))))
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = os.path.dirname(os.path.dirname(
+#     os.path.dirname(os.path.abspath(__file__))))
 
-SECRET_KEY = 'snzh3$0g@-=ck&6p)3mia83%hqq)ak^0^q_w^^39+!kbw4q6jx'
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = False
-    # SECRET_KEY = config('SECRET_KEY')
-    SECRET_KEY = 'snzh3$0g@-=ck&6p)3mia83%hqq)ak^0^q_w^^39+!kbw4q6jx'
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST'),
-            'PORT': ''
-        }
-    }
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
-
-# ALLOWED_HOSTS = ['*']
-    ALLOWED_HOSTS = ['soldorak.herokuapp.com']
-    STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
-    STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
-
-else:
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = True
-    ALLOWED_HOSTS = ['127.0.0.1']
-    # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = 'snzh3$0g@-=ck&6p)3mia83%hqq)ak^0^q_w^^39+!kbw4q6jx'
-    STRIPE_PUBLIC_KEY = config('STRIPE_TEST_PUBLIC_KEY')
-    STRIPE_SECRET_KEY = config('STRIPE_TEST_SECRET_KEY')
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+# SECRET_KEY = 'snzh3$0g@-=ck&6p)3mia83%hqq)ak^0^q_w^^39+!kbw4q6jx'
 
 
-# Application definition
+# # SECURITY WARNING: don't run with debug turned on in production!
+# if os.environ.get('ENV') == 'PRODUCTION':
+#     DEBUG = False
+#     # SECRET_KEY = config('SECRET_KEY')
+#     SECRET_KEY = 'snzh3$0g@-=ck&6p)3mia83%hqq)ak^0^q_w^^39+!kbw4q6jx'
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': config('DB_NAME'),
+#             'USER': config('DB_USER'),
+#             'PASSWORD': config('DB_PASSWORD'),
+#             'HOST': config('DB_HOST'),
+#             'PORT': ''
+#         }
+#     }
+#     db_from_env = dj_database_url.config(conn_max_age=500)
+#     DATABASES['default'].update(db_from_env)
+
+# # ALLOWED_HOSTS = ['*']
+#     ALLOWED_HOSTS = ['soldorak.herokuapp.com']
+#     STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
+#     STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
+
+# else:
+#     # SECURITY WARNING: don't run with debug turned on in production!
+#     DEBUG = True
+#     ALLOWED_HOSTS = ['127.0.0.1']
+#     # SECURITY WARNING: keep the secret key used in production secret!
+#     SECRET_KEY = 'snzh3$0g@-=ck&6p)3mia83%hqq)ak^0^q_w^^39+!kbw4q6jx'
+#     STRIPE_PUBLIC_KEY = config('STRIPE_TEST_PUBLIC_KEY')
+#     STRIPE_SECRET_KEY = config('STRIPE_TEST_SECRET_KEY')
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'crispy_forms',
-    'django_countries',
-    'stripe',
-
-    'core'
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-]
-
-INTERNAL_IPS = ['127.0.0.1']
-
-ROOT_URLCONF = 'djecommerce.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            # Cette ligne ajoute le dossier templates/ à la racine du projet
-            os.path.join(BASE_DIR, 'templates')
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'djecommerce.wsgi.application'
-
-LANGUAGE_CODE = 'fr-fr'  # us-us
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+# # Application definition
 
 
-# Static files settings
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static_in_env'),
+#     'django.contrib.sites',
+#     'allauth',
+#     'allauth.account',
+#     'allauth.socialaccount',
+#     'crispy_forms',
+#     'django_countries',
+#     'stripe',
 
-                    )
+#     'core'
+# ]
+
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+# ]
+
+# INTERNAL_IPS = ['127.0.0.1']
+
+# ROOT_URLCONF = 'djecommerce.urls'
+
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [
+#             # Cette ligne ajoute le dossier templates/ à la racine du projet
+#             os.path.join(BASE_DIR, 'templates')
+#         ],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
+# WSGI_APPLICATION = 'djecommerce.wsgi.application'
+
+# LANGUAGE_CODE = 'fr-fr'  # us-us
+# TIME_ZONE = 'UTC'
+# USE_I18N = True
+# USE_L10N = True
+# USE_TZ = True
+
+# # Static files (CSS, JavaScript, Images)
+
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/media/'
+# # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+# # STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+# # MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 
-# Auth
+# # Static files settings
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend'
-)
-SITE_ID = 1
-LOGIN_REDIRECT_URL = '/'
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static_in_env'),
 
-# CRISPY FORMS
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+#                     )
 
 
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
-]
+# # Auth
+
+# AUTHENTICATION_BACKENDS = (
+#     'django.contrib.auth.backends.ModelBackend',
+#     'allauth.account.auth_backends.AuthenticationBackend'
+# )
+# SITE_ID = 1
+# LOGIN_REDIRECT_URL = '/'
+
+# # CRISPY FORMS
+
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
-# Production
+# AUTH_PASSWORD_VALIDATORS = [
+#     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+#     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+#     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+#     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
+# ]
 
-# Activate Django-Heroku.
-# django_heroku.settings(locals())
+
+# # Production
+
+# # Activate Django-Heroku.
+# # django_heroku.settings(locals())
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -1,41 +1,41 @@
-# from .base import *
-# import dj_database_url
+from .base import *
+import dj_database_url
 
-# DEBUG = False
+DEBUG = False
 # ALLOWED_HOSTS = ['*']
-# # ALLOWED_HOSTS = ['soldorak.herokuapp.com']
+ALLOWED_HOSTS = ['soldorak.herokuapp.com']
 
 
-# AUTH_PASSWORD_VALIDATORS = [
-#     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
-# ]
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
+]
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': ''
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': ''
+    }
+}
 
-# STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
-# STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
+STRIPE_PUBLIC_KEY = config('STRIPE_LIVE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_LIVE_SECRET_KEY')
 
-# # Static files settings
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# Static files settings
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-# # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static_in_env'),
-# )
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static_in_env'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
